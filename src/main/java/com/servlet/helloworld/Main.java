73ef928ef57a3763764c73e62b91c6c1cae5bd0a
nhatnguyen <minhnhatict@gmail.com>
2019-09-21 21:03:46 +0700

    wfewf

---
+++ src/main/java/com/servlet/helloworld/Main.java
@@ -0,0 +1,4 @@
+package com.servlet.helloworld;
+
+public class Main {
+}
---
+++ src/main/java/com/servlet/helloworld/servlet/ServletWithAnnotations.java
@@ -0,0 +1,17 @@
+package com.servlet.helloworld.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/hello")
+public class ServletWithAnnotations extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest reqest, HttpServletResponse response) 
+			throws ServletException, IOException {
+		response.getWriter().println("Hello World!");
+	}
+}
