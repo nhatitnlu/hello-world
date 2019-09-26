361d580ed60301e0478616fed222e2b854bd7a33
nhatnguyen <minhnhatict@gmail.com>
2019-09-26 12:43:45 +0700

    test webhook

--- src/main/java/com/servlet/helloworld/servlet/ServletWithAnnotations.java
+++ src/main/java/com/servlet/helloworld/servlet/ServletWithAnnotations.java
@@ -12,6 +12,6 @@ public class ServletWithAnnotations extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest reqest, HttpServletResponse response) 
 			throws ServletException, IOException {
-		response.getWriter().println("Hello World! Edited by auto deploy more r");
+		response.getWriter().println("Hello World! Edited by auto deploy more dr");
 	}
 }
