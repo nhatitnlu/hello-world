081c6c277d4a060096e877df04f8ee1172336dae
nhatnguyen <minhnhatict@gmail.com>
2019-10-07 23:05:09 +0700

    test auto build

--- src/main/java/com/servlet/helloworld/servlet/ServletWithAnnotations.java
+++ src/main/java/com/servlet/helloworld/servlet/ServletWithAnnotations.java
@@ -12,6 +12,6 @@ public class ServletWithAnnotations extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest reqest, HttpServletResponse response) 
 			throws ServletException, IOException {
-		response.getWriter().println("Hello   d World! Edited by cauto fd ff     deploy dr");
+		response.getWriter().println("Hello   d World! Edited by cauto fd ff     ddeploy dr");
 	}
 }
