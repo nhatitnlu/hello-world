14ef010fe14d28d969610cdca2f0f879e5381597
nhatnguyen <minhnhatict@gmail.com>
2019-10-08 21:52:18 +0700

    test auto build

--- src/main/java/com/servlet/helloworld/servlet/ServletWithAnnotations.java
+++ src/main/java/com/servlet/helloworld/servlet/ServletWithAnnotations.java
@@ -12,6 +12,6 @@ public class ServletWithAnnotations extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest reqest, HttpServletResponse response) 
 			throws ServletException, IOException {
-		response.getWriter().println("Hello  d Wor dd ld );
+		response.getWriter().println("Hello  d Wor dd ld  );
 	}
 }
